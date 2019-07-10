fd83346e9e69baa9ad4063e7c9ca16129f371370
James Rudd <james.rudd45@gmail.com>
2019-07-10 14:03:22 +1000

    Started working out the code for creating the character choice boxes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,9 @@
 $("#gameStart").on('click', function () {
     $('#gameStart').css({ display: "none" });
     $('#playArea').css({ display: "inherit" });
+    var character = $("<div>");
+    character.addClass("playerChoice");
+
+    $('#charChoice').append(character);
+    $('.playerChoice').append("<img id='yoda' src='./assets/images/yoda.jpg' />");
 });
