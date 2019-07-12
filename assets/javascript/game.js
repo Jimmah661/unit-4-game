105fc1287a51691bdc52ab538d296ee70ebbebf0
James Rudd <james.rudd45@gmail.com>
2019-07-12 21:27:16 +1000

    got the attack system working, need to make the functions to affect all players now

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -19,11 +19,21 @@ var characterList = {
     },
     vader: {
         hp: 100,
-        atk: 40,
+        atk: 6,
         catk: 25,
         img: "<img class='portrait' src='./assets/images/vader.jpg' />"
     },
 };
+
+var playerStats;
+var defenderStats = {
+    hp: 0,
+    atk: 0,
+    catk: 0
+};
+var enemyStorage;
+var smackDown = 0;
+var gameActive = false;
 
 var playFunctions = {
     generate: function (x) {
@@ -36,12 +46,12 @@ var playFunctions = {
             character.append("<p>" + name.charAt(0).toUpperCase() + name.slice(1) + "</p>");
             character.append(Object(x)[name].img);
             character.append(Object(x)[name].hp)
-            $('#charChoice').prepend(character);
+            $('#charChoice').append(character);
         }
     }
 };
 
-
+// I'll come back to the start screen later
 
 // $("#gameStart").on('click', function () {
 
@@ -55,7 +65,11 @@ var playFunctions = {
 //     playFunctions.generate(characterList);
 // });
 
+// Generate all the possible player choices
 playFunctions.generate(characterList);
+
+// beginnings of the character selection
+// This will separate out into Player and Enemies
 $('#vader').on('click', function () {
     $('#vader').removeClass('playerChoice');
     $('#vader').addClass('chosenPlayer');
@@ -63,6 +77,31 @@ $('#vader').on('click', function () {
     $('.playerChoice').detach().appendTo('#availEnemies');
     $('.playerChoice').addClass('enemy');
     $('.playerChoice').removeClass('playerChoice');
-    console.log('test');
+    playerStats = characterList.vader;
+})
+
+// Beginnings of defender choices
+// Lets start with Maul
+$('#maul').on('click', function () {
+    // Move that enemy to the defender section
+    $('#maul').detach().appendTo('#defender');
+    // Move his stats to the enemy stats list
+    defenderStats = characterList.maul;
+    // Hide other enemies
+    $('#availEnemies').hide();
+    gameActive = true;
+})
+
+$('#smackThat').click(function () {
+    console.log(defenderStats.hp);
+    defenderStats.hp = defenderStats.hp - playerStats.atk;
+    playerStats.atk = playerStats.atk + 6;
+    // console.log(playerStats);
+    console.log(defenderStats.hp);
+    if (gameActive = true && defenderStats.hp <= 0) {
+        $('#defender').empty();
+        smackDown++;
+        $('#availEnemies').show();
+    }
 })
 
